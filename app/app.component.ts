74898b335bcb97bf706f94367f10310717a6476d
Gabriel Ajabahian <gajabahian@ripe.net>
2016-09-27 16:56:30 +0200

    Adding declaration

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,16 +6,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent { 
     // [ ] means property biding - Component to Dom
-    // ( ) means event binding - Dom to C
-
+    // ( ) means event binding - Dom to Component
     title = 'Customer App';
     name = 'Gabriel';
     color = 'green'; 
     customers = [
-        {id: 1, name: 'Gabriel'},
-        {id: 2, name: 'Nancy'},
-        {id: 3, name: 'Emmer'},
-        {id: 4, name: 'Sally'}
+        {id:1, name:'Gabriel'},
+        {id:2, name:'Sally'},
+        {id:3, name:'Ton'},
+        {id:4, name:'Dadepo'}
     ];
     changeColor() { 
         this.color = this.color === 'green' ? 'red' : 'green';
