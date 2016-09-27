d2d0c7b53b4221e363d760e18a56599df571f00c
Gabriel Ajabahian <gajabahian@ripe.net>
2016-09-27 16:25:00 +0200

    Adding Forms, and listing

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,14 +5,19 @@ import { Component } from '@angular/core';
     templateUrl: 'app/app.component.html'
 })
 export class AppComponent { 
-    title = 'Customer App';
-    name = 'Gabriel';
-    color = 'green'; 
-
     // [ ] means property biding - Component to Dom
     // ( ) means event binding - Dom to C
 
+    title = 'Customer App';
+    name = 'Gabriel';
+    color = 'green'; 
+    customers = [
+        {id: 1, name: 'Gabriel'},
+        {id: 2, name: 'Nancy'},
+        {id: 3, name: 'Emmer'},
+        {id: 4, name: 'Sally'}
+    ];
     changeColor() { 
-        this.color = this.color === 'blue' ? 'red' : 'blue';
+        this.color = this.color === 'green' ? 'red' : 'green';
     }
 }
