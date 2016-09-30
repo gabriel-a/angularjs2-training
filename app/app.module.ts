55a29a3e765296a269fedbfb59ff193f3af0f2e0
Gabriel Ajabahian <gajabahian@ripe.net>
2016-09-30 11:31:25 +0200

    I got Async working with lists

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
+
 
 //custom components (Directives)
 import { CustomerComponent } from './customer/customer.component';
 import { CustomersComponent } from './customer/customers.component';
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule ],
+  imports: [ BrowserModule, FormsModule, HttpModule ],
   declarations: [ AppComponent, CustomersComponent, CustomerComponent ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [ AppComponent ],
 })
 export class AppModule { }
--- app/customer/customer.service.ts
+++ app/customer/customer.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+
+const CUSTOMER_URL = 'app/customers.json';
 
 @Injectable()
 export class CustomerService {
 
-    constructor() { }
+    constructor(private _http: Http) { }
 
     getCustomers(){
-        return  [
-            {id:1, name:'Gabriel'},
-            {id:2, name:'Sally'},
-            {id:3, name:'Ton'},
-            {id:4, name:'Dadepo'}
-        ];
+        return this._http.get(CUSTOMER_URL)
+                        // ...and calling .json() on the response to return data
+                         .map((res:Response) => res.json())
+                         //...errors if any
+                         .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
     }
 }
